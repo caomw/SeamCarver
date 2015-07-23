5fae59f80436bf92838d2457748bf0611098c262
Max <maxw14k@gmail.com>
2015-07-23 17:42:46 -0400

    created separate class for helper static methods
    most methods moved to separate class, cleaned code

--- SeamCarver.java
+++ SeamCarver.java
@@ -1,8 +1,5 @@
-import java.awt.Color;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Set;
 
 public class SeamCarver {
 	
@@ -156,7 +153,6 @@ public class SeamCarver {
 	   return seamArr;
 	   
    }  
-  
    private void relax(int row, int col, double[][] distTo, int[][] edgeTo, double[][] energyMatrix){
 		 //this function "relaxes" a vertex: it checks whether it can offer a shorter distance to any node it can reach
 		   //by adding its energy to the energy of its connected node and checking whether it is better than the best the
@@ -213,162 +209,8 @@ public class SeamCarver {
 		   }
 		   
 	   }
-//   private static Picture transpose(Picture old){
-//	   Picture temp = new Picture(old.height(),old.width());
-//	   
-//	   for(int i = 0; i < old.height(); i++){
-//		   for(int j = 0; j < old.width(); j++){
-//			   temp.set(i, j, old.get(j, i));
-//		   }
-//	   }
-//	   return temp;
-//   }
-//   private static double[][] transpose(double [][] m){
-//       double[][] temp = new double[m[0].length][m.length];
-//       for (int i = 0; i < m.length; i++)
-//           for (int j = 0; j < m[0].length; j++)
-//               temp[j][i] = m[i][j];
-//       return temp;
-//   }
-//   public static Integer[] tran2origIndex(int width, int height, Integer[] linIndexT){
-//		  //Maps linear indices in a transposed image to linear indices in non-transposed ("original") image
-//		   //takes in WIDTH and HEIGHT of ORIGINAL NON-TRANSPOSED IMAGE and the linear indices in the transposed image
-//		   
-//		   int[] origInd = new int[linIndexT.length];
-//		   int[] subInOrig = new int[2];
-//		   int[] subInTransp = new int[2];
-//
-//		   for(int i = 0; i < linIndexT.length; i++){
-//			   //convert back to 2D subscripts in transposed image
-//			   subInTransp = ind2sub(width, height, linIndexT[i]);
-//			   	   
-//			   //swap for subscripts in original
-//			   subInOrig[0] = subInTransp[1];
-//			   subInOrig[1] = subInTransp[0];
-//			   
-//			   // go from 2D subscripts in orignal image, to linear indices in original
-//			   int newLinearIndeces = sub2ind(subInOrig[0],subInOrig[1], width);
-//			   
-//			   origInd[i] = newLinearIndeces;
-////			   System.out.println("Linear Index transposed image: " + linIndexT[i]);
-////			   System.out.println("Subscript in transposed image: " + Arrays.deepToString(int2Int(subInTransp)));
-////			   System.out.println("Subscript in original image: " + Arrays.deepToString(int2Int(subInOrig)));
-////			   System.out.println("Linear Index in original image: " + newLinearIndeces);
-//		   }
-//		   return int2Int(origInd);
-//	   }
-//   
-//   private static double deltaY(int row, int col, Picture pic){
-//	   //outputs the squared difference between the pixel above and below pixel at (row,col)
-//	   //if a pixel is on top/bottom row, we "wrap around the image" and grab pixel on other side
-//	   Color a;
-//	   Color b;
-//	   
-//	   //bottom wall
-//	   if(row == (pic.height()-1))  { a = pic.get(col, row-1); b = pic.get(col, 0);} 
-//	   //top wall
-//	   else if(row == 0)  			{ a = pic.get(col, pic.height()-1); b = pic.get(col, row+1); }
-//	   //between top/bottom
-//	   else 						{ a = pic.get(col, row-1); b = pic.get(col, row+1); }
-//	   
-//	   double deltaR = Math.pow((a.getRed()   - b.getRed()),  2);
-//	   double deltaG = Math.pow((a.getGreen() - b.getGreen()),2);
-//	   double deltaB = Math.pow((a.getBlue()  - b.getBlue()), 2);
-//	   
-//	   return deltaR + deltaG + deltaB;   
-//   }
-//   private static double deltaX(int row, int col, Picture pic){
-//	   //outputs the squared difference between the pixel to the left and right of pixel at (row,col)
-//	   //if a pixel is on left/right column, we "wrap around the image" and grab pixel on other side
-//	   Color a;
-//	   Color b;
-//	   
-//	   //right wall
-//	   if(col == (pic.width()-1))  { a = pic.get(col-1, row); b = pic.get(0, row);} 
-//	   //left wall
-//	   else if(col == 0)  			{ a = pic.get(pic.width()-1, row); b = pic.get(col+1, row); }
-//	   //between left/right
-//	   else 						{ a = pic.get(col-1, row); b = pic.get(col+1, row); }
-//	   
-//	   double deltaR = Math.pow((a.getRed()   - b.getRed()),  2);
-//	   double deltaG = Math.pow((a.getGreen() - b.getGreen()),2);
-//	   double deltaB = Math.pow((a.getBlue()  - b.getBlue()), 2);
-//	   
-//	   return deltaR + deltaG + deltaB;   
-//   }
-//   private static double[][] fillEnergyMatrix(Picture pic){
-//	  //Populate energyMatrix with "dual-gradient energy" function
-//	   
-//	   //energyMatrix: [ row 0;  <-- row is length Width-1
-//	   //  				 row 1;
-//	   //				 row Height-1;
-//	   
-//	   double[][] matrix = new double[pic.height()][pic.width()];
-//	   for(int row = 0; row<pic.height(); row++){
-//		   for (int col = 0; col<pic.width(); col++){
-//			   matrix[row][col] = energy(col,row, pic);
-//		   }
-//	   }
-//	   return matrix; 
-//   }   
-//   private static Picture copyPic(Picture a){
-//	   Picture img = new Picture(a.width(), a.height());
-//	   //make defensive copy
-//	   for(int i = 0; i<a.width(); i++){
-//		   for(int j = 0; j<a.height(); j++){
-//			   img.set(i, j, a.get(i, j));
-//		   }
-//	   }
-//	   return img;
-//   }
-//   private static int minIndex(double[] a){
-//	   //copy elements of array into new array
-//	   //find minimum element while keeping track of index and return index
-//	   double[] copyArr = new double[a.length];
-//	   for(int i = 0; i < a.length; i++){ copyArr[i] = a[i];}
-//	   
-//	   int idx = -1;
-//	   double d= Double.POSITIVE_INFINITY;
-//	    for(int i = 0; i < copyArr.length; i++)
-//	        if(copyArr[i] < d) { d = copyArr[i];  idx = i; }
-//	    
-//	    return idx;
-//   }  
-//   public static int sub2ind(int row, int column, int width){
-//		return (width*row + column);	
-//	}
-//   public static int[] ind2sub(int height, int width, int LinearIndex){
-//	   //maps indeces to 2D subscripts
-//	   int row = LinearIndex/width;
-//	   int col = LinearIndex - (width*row);
-//	   int[] a = {row,col};
-//	   return a;
-//   }
-//   public static Integer[] int2Int(int[] a){
-//	   Integer[] b = new Integer[a.length];
-//	   
-//	   for(int i = 0; i < a.length; i++){
-//		   b[i] =  a[i];
-//	   }
-//	   return b;
-//   }
-//   public static void printMatrix(int[][] m){
-//	   for(int i = 0; i<m.length;i++){
-//		   System.out.println();
-//		   for(int j = 0; j<m[i].length; j++){
-//			   System.out.print(m[i][j] + " ");
-//		   }
-//	   }
-//   }
-//   public static void printMatrix(double[][] m){
-//	   for(int i = 0; i<m.length;i++){
-//		   System.out.println();
-//		   for(int j = 0; j<m[i].length; j++){
-//			   System.out.print(m[i][j] + " ");
-//		   }
-//	   }
-//   }
-//   public static void main(String args[]){
+
+   public static void main(String args[]){
 	   String[] filename = {"3x4.png"};
 	   SeamCarver a = new SeamCarver(new Picture(filename[0]));
 	   
@@ -381,8 +223,8 @@ public class SeamCarver {
 	 		//PrintSeams.main(new String[] {"6x5.png"});
 	 		Integer[] v = f.findVerticalSeam();
 	 		Integer[] h = f.findHorizontalSeam();
-	 		Integer[] vSeam = {sub2ind(0,3,6), sub2ind(1,4,6), sub2ind(2,3,6), sub2ind(3,2,6), sub2ind(4,2,6)};
-	 		Integer[] hSeam = {sub2ind(2,0,6), sub2ind(2,1,6), sub2ind(1,2,6), sub2ind(2,3,6), sub2ind(1,4,6), sub2ind(2,5,6)};
+	 		Integer[] vSeam = {SChelper.sub2ind(0,3,6), SChelper.sub2ind(1,4,6), SChelper.sub2ind(2,3,6), SChelper.sub2ind(3,2,6), SChelper.sub2ind(4,2,6)};
+	 		Integer[] hSeam = {SChelper.sub2ind(2,0,6), SChelper.sub2ind(2,1,6), SChelper.sub2ind(1,2,6), SChelper.sub2ind(2,3,6), SChelper.sub2ind(1,4,6), SChelper.sub2ind(2,5,6)};
 	 		System.out.println("    Correct Vertical Seam on 6x5:    " + Arrays.deepEquals(v, vSeam));
 	 		System.out.println("    Correct Horizontal Seam on 6x5:  " + Arrays.deepEquals(h, hSeam));
 
@@ -390,8 +232,8 @@ public class SeamCarver {
 	 		//PrintSeams.main(new String[] {"3x4.png"});
 	 		Integer[] v1 = a.findVerticalSeam();
 	 		Integer[] h1 = a.findHorizontalSeam();
-	 		Integer[] vSeam1 = {sub2ind(0,0,3), sub2ind(1,0,3), sub2ind(2,0,3), sub2ind(3,0,3)};
-	 		Integer[] hSeam1 = {sub2ind(0,0,3), sub2ind(0,1,3), sub2ind(0,2,3)};
+	 		Integer[] vSeam1 = {SChelper.sub2ind(0,0,3), SChelper.sub2ind(1,0,3), SChelper.sub2ind(2,0,3), SChelper.sub2ind(3,0,3)};
+	 		Integer[] hSeam1 = {SChelper.sub2ind(0,0,3), SChelper.sub2ind(0,1,3), SChelper.sub2ind(0,2,3)};
 	 		System.out.println("    Correct Vertical Seam on 3x4:    " + Arrays.deepEquals(v1, vSeam1)); 	
 	 		System.out.println("    Correct Horizont Seam on 3x4:    " + Arrays.deepEquals(h1, hSeam1));
 	 
@@ -408,72 +250,72 @@ public class SeamCarver {
 	 //test sub2ind
 	 	   System.out.println("\nTest sub2ind  : for a Height x Width: 3x3 matrix");
 	 	   int width = 3;
-	 	   System.out.println(0 == sub2ind(0,0,width));
-	 	   System.out.println(5 == sub2ind(1,2,width));
-	 	   System.out.println(8 == sub2ind(2,2,width));
-	 	   System.out.println(0 == sub2ind(0,0,width));
-	 	   System.out.println(5 == sub2ind(1,2,width));
+	 	   System.out.println(0 == SChelper.sub2ind(0,0,width));
+	 	   System.out.println(5 == SChelper.sub2ind(1,2,width));
+	 	   System.out.println(8 == SChelper.sub2ind(2,2,width));
+	 	   System.out.println(0 == SChelper.sub2ind(0,0,width));
+	 	   System.out.println(5 == SChelper.sub2ind(1,2,width));
 	 	   System.out.println("\nTest ind2sub: for a Height x Width: 3x4 matrix");
 	 	   int height = 3;
 	 	   width = 4;
 	 	   int[] j = new int[2];
 	 	   j[0] = 0; j[1] = 0;
-	 	   int[] b = ind2sub(height,width,0);
+	 	   int[] b = SChelper.ind2sub(height,width,0);
 	 	   System.out.println((j[0] == b[0]) && (j[1] == b[1]));
 	 	   
 	 	   j[0] = 1; j[1] = 1;
-	 	   b = ind2sub(height,width,5);
+	 	   b = SChelper.ind2sub(height,width,5);
 	 	   System.out.println((j[0] == b[0]) && (j[1] == b[1]));
 	 	   
 	 	   j[0] = 2; j[1] = 0;
-	 	   b = ind2sub(height,width,8);
+	 	   b = SChelper.ind2sub(height,width,8);
 	 	   System.out.println((j[0] == b[0]) && (j[1] == b[1]));
 	 	   
 	 	   //minIndex
 	 	   System.out.println("\nTest minIndex");
 	 	   double[] m = {0.5, 0.65, 1.5, 0.03};
-	 	   System.out.println(3 == minIndex(m));
+	 	   System.out.println(3 == SChelper.minIndex(m));
 	 	   m[0] = 0.0;
 	 	   
 	 	   
-	 	   System.out.println(0 == minIndex(m));
+	 	   System.out.println(0 == SChelper.minIndex(m));
 	   
 	 	  System.out.println("\nTest deltaX/Y on 3x4.png");
 	// test deltaX deltaY	   
 	 		   	//top left
 	 		// deltaX
-	 		   	System.out.println("DX Top Left, Should be 10404:     " + (10404 == deltaX(0,0,a.picture())));
+	 		   	System.out.println("DX Top Left, Should be 10404:     " + (10404 == SChelper.deltaX(0,0,a.picture())));
 	 		// deltaY
-	 		   	System.out.println("DY Top Left, Should be 10404:     " + (10404 == deltaY(0,0,a.picture())));
+	 		   	System.out.println("DY Top Left, Should be 10404:     " + (10404 == SChelper.deltaY(0,0,a.picture())));
 	 		// Energy
 	 		   	System.out.println("EN Top Left, Should be 10404:     " + (20808 == energy(0,0,a.picture()) ));
 	 		   	//top right
-	 		   	System.out.println("DX Top Right, Should be 10404:    " + (10404 == deltaX(0,(a.width()-1),a.picture()) ) );
-	 		   	System.out.println("DY Top Right, Should be 10404:    " + (10404 == deltaY(0,(a.width()-1),a.picture()) ) );
-	 		   	System.out.println("EN Top Right, Should be 20808:    " + (20808 == energy((a.width()-1),0,a.picture()) ) );
+	 		   	System.out.println("DX Top Right, Should be 10404:    " + (10404 == SChelper.deltaX(0,(a.width()-1),a.picture()) ) );
+	 		   	System.out.println("DY Top Right, Should be 10404:    " + (10404 == SChelper.deltaY(0,(a.width()-1),a.picture()) ) );
+	 		   	System.out.println("EN Top Right, Should be 20808:    " + (20808 == SChelper.energy((a.width()-1),0,a.picture()) ) );
 	 		   	//bottom left
-	 		   	System.out.println("DX Bottom Left, Should be 10404:  " + (10404 == deltaX(a.height()-1,0,a.picture())));
-	 		   	System.out.println("DY Bottom Left, Should be 10404:  " + (10404 == deltaY(a.height()-1,0,a.picture())));
+	 		   	System.out.println("DX Bottom Left, Should be 10404:  " + (10404 == SChelper.deltaX(a.height()-1,0,a.picture())));
+	 		   	System.out.println("DY Bottom Left, Should be 10404:  " + (10404 == SChelper.deltaY(a.height()-1,0,a.picture())));
 	 		   	System.out.println("EN Bottom Left, Should be 20808:  " + (20808 == energy(0,a.height()-1,a.picture())));
 	 		   	//bottom right
-	 		   	System.out.println("DX Bottom Right, Should be 10404: " + (10404 == deltaX(a.height()-1,a.width()-1,a.picture())));
-	 		   	System.out.println("DY Bottom Right, Should be 10404: " + (10816 == deltaY(a.height()-1,a.width()-1,a.picture())));
+	 		   	System.out.println("DX Bottom Right, Should be 10404: " + (10404 == SChelper.deltaX(a.height()-1,a.width()-1,a.picture())));
+	 		   	System.out.println("DY Bottom Right, Should be 10404: " + (10816 == SChelper.deltaY(a.height()-1,a.width()-1,a.picture())));
 	 		   	System.out.println("EN Bottom Right, Should be 21220: " + (21220 == energy(a.width()-1,a.height()-1,a.picture())));
 	 		   	//middle	
-	 		   	System.out.println("DX Middle, Should be 10404:       " + (41616 == deltaX(1,1,a.picture())));
-	 		   	System.out.println("DY Middle, Should be 10404:       " + (10609 == deltaY(1,1,a.picture())));
+	 		   	System.out.println("DX Middle, Should be 10404:       " + (41616 == SChelper.deltaX(1,1,a.picture())));
+	 		   	System.out.println("DY Middle, Should be 10404:       " + (10609 == SChelper.deltaY(1,1,a.picture())));
 	 		   	System.out.println("EN Middle, Should be 52225:       " + (52225 == energy(1,1,a.picture())));	 
 	 		   	
 	 // Test tran2origIndex
 	 	System.out.println("\nTest tran2origIndex: 3x3 matrix");	 
 	 	
-	 	Integer[] regIndices   = { sub2ind(0,0,3), sub2ind(0,1,3), sub2ind(0,2,3) };
-	 	Integer[] transIndices = { sub2ind(0,0,3), sub2ind(1,0,3), sub2ind(2,0,3) };
+	 	Integer[] regIndices   = { SChelper.sub2ind(0,0,3), SChelper.sub2ind(0,1,3), SChelper.sub2ind(0,2,3) };
+	 	Integer[] transIndices = { SChelper.sub2ind(0,0,3), SChelper.sub2ind(1,0,3), SChelper.sub2ind(2,0,3) };
 	 	System.out.println("    regIndices:" + Arrays.toString(regIndices));
 	 	System.out.println("    transIndices:" + Arrays.toString(transIndices));
-	 	Integer[] fromMethod = tran2origIndex(3,3,regIndices);
+	 	Integer[] fromMethod = SChelper.tran2origIndex(3,3,regIndices);
 	 	System.out.println("    transIndices from method:" + Arrays.toString(fromMethod));
-	 	System.out.println("    transpose Indeces mapping corectly to regular indeces: " + Arrays.deepEquals(tran2origIndex(3,3,regIndices), transIndices) );
+	 	System.out.println("    transpose Indeces mapping corectly to regular indeces: " + Arrays.deepEquals(SChelper.tran2origIndex(3,3,regIndices), transIndices) );
    
    }
 }
